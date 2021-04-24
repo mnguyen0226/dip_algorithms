55820538c3645bd251926622532346dcceb6ce88
mnguyen0226 <mnguyen0226@vt.edu>
2021-04-24 12:10:57 -0400

    add code and report

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # OpenCV Digital Image Processing Techniques
 This repository contains various digital image processing techniques that I learned in ECE 4580. The code is implemented in Python >= 3 and OpenCV. The techniques will be used in Image Augmentation ML Project
+## Honor Code @VT Warning:
+You know what VT's Honor Code is, Hokies :). Don't do it. You have been warned.
 ## Technique One:
+- Image Augmentation:
+- Image Shearing:
 
 ### Result:
+Can be found in:
 ### How to run:
-
-### Report:
+```
+python imageShear.py
+python imageTranslation.py
+```
+### Report (password will be given to related individuals only):
 
 ## Technique Two:
 
 ### Result:
 ### How to run:
 
-### Report:
+### Report (password will be given to related individuals only):
 
 
 ## Technique Three:
@@ -20,7 +28,7 @@ This repository contains various digital image processing techniques that I lear
 ### Result:
 ### How to run:
 
-### Report:
+### Report (password will be given to related individuals only):
 
 
 ## Technique Four:
@@ -28,4 +36,4 @@ This repository contains various digital image processing techniques that I lear
 ### Result:
 ### How to run:
 
-### Report:
+### Report (password will be given to related individuals only):
---
+++ technique one/src/imageShear.py
@@ -0,0 +1,54 @@
+"""
+Author: Minh T. Nguyen
+2/3/2021
+ECE 4580: Digital Image Processing
+
+TODO 2: Write the image shear function with the shearing factor
+"""
+import cv2 as cv
+import numpy as np
+
+# background is black
+def imageShear4e(f, sv, sh):
+    h,w = f.shape[:2]
+    transMat = np.array([[1, sv, 0], [sh, 1, 0]])
+    out_img = np.zeros(f.shape, dtype = 'uint8')
+    for m in range(h):
+        for n in range(w):
+            origin_pos_x = n
+            origin_pos_y = m
+            origin_pos_xy = np.array([origin_pos_x, origin_pos_y, 1])
+
+            new_pos_xy = np.dot(transMat, origin_pos_xy)
+            new_pos_x = int(new_pos_xy[0])
+            new_pos_y = int(new_pos_xy[1])
+        
+            if 0 < new_pos_x < w and 0 < new_pos_y < h:
+                out_img[new_pos_y, new_pos_x] = f[m,n]
+    return out_img  
+
+def main():
+    print("Running")
+    
+    # Read in the image of the girl
+    img = cv.imread("girl.tif")
+    
+    # Just show out the original
+    cv.imshow("Original", img)
+
+    # Create a shear image with black background
+    out_img_sheared1 = imageShear4e(img, 0.5, 0)
+    cv.imshow("Shear Image (sv, sh) = (0.5, 0)", out_img_sheared1)
+
+    # Create a shear image with black background
+    out_img_sheared2 = imageShear4e(img, 0, -0.75)
+    cv.imshow("Shear Image (sv, sh) = (0, -0.75)", out_img_sheared2)
+
+    # Create a shear image with black background
+    out_img_sheared3 = imageShear4e(img, 0.5, -0.75)
+    cv.imshow("Shear Image (sv, sh) = (0.5, -0.75)", out_img_sheared3)
+
+    cv.waitKey(0)
+
+if __name__ == "__main__":
+    main()
